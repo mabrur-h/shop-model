895ae68ccf9e8f1144677b7fc84bdacd81052b5e
mabrur-h <mabrur.h11@gmail.com>
2021-06-03 11:28:36 +0500

    🛠Add and configure models

---
+++ main.js
@@ -0,0 +1,41 @@
+const { Sequelize } = require('sequelize');
+const UserModel = require('./models/UserModel');
+const CategoryModel = require('./models/CategoryModel');
+const ProductModel = require('./models/ProductModel');
+
+const sequelize = new Sequelize('postgres://postgres:pgadmin@localhost:5432/shop_model', {
+    logging: sql => console.log("SQL: ", sql)
+});
+
+async function main() {
+    try {
+        await sequelize.authenticate();
+        console.log('Connected to the database');
+
+        let db = {};
+        db.users = await UserModel(Sequelize, sequelize);
+        db.categories = await CategoryModel(Sequelize, sequelize);
+        db.products = await ProductModel(Sequelize, sequelize);
+
+        db.categories.hasMany(db.products, {
+            foreignKey: {
+                name: "category_id",
+                allowNull: false
+            }
+        })
+
+        db.products.belongsTo(db.categories, {
+            foreignKey: {
+                name: "category_id",
+                allowNull: false
+            }
+        })
+
+        await sequelize.sync({ force: true });
+
+    } catch (e) {
+        console.log('Connection error: ', e);
+    }
+}
+
+main().then();
